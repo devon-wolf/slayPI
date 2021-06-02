cda1dc8ee271afa37b8827ea18de3999ef48d356
Devon Wolfkiel <devonwolf@tutanota.com>
2021-06-01 18:15:44 -0700

    Import types for libraries, update types for scraper functions, and add function to get the infoboxes for all characters in the category used

--- src/lib/services/scoobyScraper.ts
+++ src/lib/services/scoobyScraper.ts
@@ -1,42 +1,52 @@
-import { response } from "express";
+import * as cheerio from 'cheerio';
+import * as request from 'superagent';
 
-const cheerio = require('cheerio');
-const request = require('superagent');
-
-const buffyURL : string = 'https://buffy.fandom.com';
+const buffyURL = 'https://buffy.fandom.com';
 
 const scoobyHTML = () => {
 	return request.get(`${buffyURL}/wiki/Category:Scooby_Gang`)
-		.then((response : Response) => response.text)
+		.then(response => response.text)
 		.catch(console.error);
 }
 
 const getScoobyLinks = async () => {
 	const categoryPage = await scoobyHTML();
-	const $ = cheerio.load(categoryPage);
-
-	const scoobyLinkArray : [string?] = [];
-
-	$('.category-page__member-link').each((i : number, el : HTMLAnchorElement) => scoobyLinkArray.push($(el).attr('href')));
-
-	return scoobyLinkArray;
+	if (categoryPage) {
+		const $ = cheerio.load(categoryPage);
+		const scoobyLinkArray : string[] = [];
+		$('.category-page__member-link').each((i, el) => {
+			const href = $(el).attr('href');
+			if (href) scoobyLinkArray.push(href);
+		});
+		return scoobyLinkArray;
+	}
 }
 
 const characterHTML = (href : string) => {
 	return request.get(`${buffyURL}${href}`)
-		.then((response : Response) => response.text)
+		.then(response => response.text)
 		.catch(console.error);
 }
 
 const getScoobyData = async (href : string) => {
 	const characterPage = await characterHTML(href);
-	const $ = cheerio.load(characterPage);
-	
-	// const character = {};
-
-	const infoBox = $('.portable-infobox')
-	return infoBox;
-	
+	if (characterPage) {
+		const $ = cheerio.load(characterPage);
+		const infoBox = $('.portable-infobox')
+		return infoBox;
+	}
 }
 
-module.exports = { getScoobyLinks, getScoobyData }
+const getAllScoobyData = async () => {
+	const allInfoBoxes : cheerio.Cheerio[] = [];
+	const scoobyLinkArray = await getScoobyLinks();
+	if (scoobyLinkArray && scoobyLinkArray.length) {
+		scoobyLinkArray.forEach(async link => {
+			const infoBox = await getScoobyData(link);
+			if (infoBox) allInfoBoxes.push(infoBox);
+		});
+		return allInfoBoxes;
+	}
+}
+
+module.exports = { getScoobyLinks, getScoobyData, getAllScoobyData }
